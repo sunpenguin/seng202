87bd23f7227d1c13e04f1c24c356f51f95316353
YaFedImYaEatIm <gondra2000@hotmail.com>
2016-09-16 12:42:29 +1200

    Made Abstract Filter Class

---
+++ src/main/java/seng202/group9/Controller/Filter.java
@@ -0,0 +1,11 @@
+package seng202.group9.Controller;
+
+import java.util.ArrayList;
+
+/**
+ * Created by fwy13 on 16/09/16.
+ */
+public abstract class Filter {
+    abstract void reset();
+    abstract ArrayList getFilteredData();
+}
