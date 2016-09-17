3218b729411a50925d44c2826036a2dbf41b8903
Michael Wilson <mrw106@uclive.ac.nz>
2016-09-17 16:02:27 +1200

    Created pie graph viewer for Airports.

--- src/main/java/seng202/group9/Controller/SceneCode.java
+++ src/main/java/seng202/group9/Controller/SceneCode.java
@@ -7,7 +7,7 @@ public enum SceneCode {
     INITIAL(""), AIRLINE_SUMMARY("airline_summary.fxml"), AIRLINE_RAW_DATA("airline_raw_data.fxml"),
     AIRPORT_SUMMARY("airport_summary.fxml"), AIRPORT_RAW_DATA("airport_raw_data.fxml"),
     ROUTE_SUMMARY("routes_summary.fxml"), ROUTE_RAW_DATA("route_raw_data.fxml"), FLIGHT_SUMMARY("flight_data_summary.fxml"),
-    FLIGHT_RAW_DATA("flight_raw_data.fxml"), ROUTE_ANALYSER("route_analyser.fxml");
+    FLIGHT_RAW_DATA("flight_raw_data.fxml"), AIRPORT_ANALYSER("airport_analyser.fxml"), ROUTE_ANALYSER("route_analyser.fxml");;
 
     private String filePath;
 
