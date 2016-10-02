2404c18d51cf549bb31a7b99d8146791d40a2292
Sunguin Peng <spe76@uclive.ac.nz>
2016-10-02 16:11:52 +1300

    Changed JSwing component in new flight path.

--- src/main/java/seng202/group9/Controller/SceneCode.java
+++ src/main/java/seng202/group9/Controller/SceneCode.java
@@ -13,7 +13,8 @@ public enum SceneCode {
     AIRPORT_ADD("airport_add_form.fxml"), AIRPORT_FILTER("airport_filter_form.fxml"), ROUTE_ADD("route_add_form.fxml"),
     ROUTE_FILTER("route_filter_form.fxml"), AIRLINE_EDIT("airline_edit_form.fxml"), AIRPORT_EDIT("airport_edit_form.fxml"),
     ROUTE_EDIT("route_edit_form.fxml"), FLIGHT_EDITOR("flight_editor_form.fxml"), DATASET_CONTROLLER("dataset_editor.fxml"), HELP("help.fxml"),
-    FLIGHT_ADD("flight_add_form.fxml"), ROUTE_BY_AIRPORT("airport_map_routes.fxml"), ROUTE_BY_EQUIP("route_by_equip.fxml");
+    FLIGHT_ADD("flight_add_form.fxml"), ROUTE_BY_AIRPORT("airport_map_routes.fxml"), ROUTE_BY_EQUIP("route_by_equip.fxml"),
+    FLIGHT_PATH_ADD("new_flight_path.fxml");
 
 
     private String filePath;
